6fbeb2396ef417b73ffc6cf8084e970cb10e930f
tmac1104 <gosling.cai@foxmail.com>
2014-05-07 19:59:16 +0800

    update test2.c

--- test/test2.c
+++ test/test2.c
@@ -1,11 +1,16 @@
 #include <string.h>
 #include <stdio.h>
+void replace(int *a,int *b);
 int main(int argc , char *argv[])
 {
 	int a = 5,b=2;
-	a=a^b;
-        b=b^a;
-        a=a^b;	
+	replace(&a,&b);	
 	printf("a=[%d],b=[%d]\n",a,b);	
 	return 0;
+}
+void replace(int *a,int *b)
+{
+	*a=*a^*b;
+        *b=*b^*a;
+        *a=*a^*b;
 }
